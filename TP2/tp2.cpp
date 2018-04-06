51055b8ae3f2fcc368a26a341f789c0a7cc61a1d
LeoGalda <leonardo.galdames@hotmail.com>
2018-04-06 20:42:36 -0300

    complicado che

--- TP2/tp2.cpp
+++ TP2/tp2.cpp
@@ -1,6 +1,9 @@
+#include <arpa/inet.h>
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
-#include <cstdlib>
+#include <stdio.h>
+#include <string.h>
 #include "Paquete.h"
 #include "Empaquetador.h"
 
@@ -34,30 +37,81 @@ void leerConfiguracion(char *arch,Empaquetador &empaquetador){
 	archivo.close();
 }
 
-void parsearArchivo(char *arch,vector<Paquete*> *empaquetador){
-	ifstream ifs;
-	ifs.open(arch, std::ifstream::binary);
-	unsigned char dato;
-	char input[4];	
-	while(!archivo.eof()){			
-		archivo.getline(input,sizeof(input));
-		string linea = input;
-		if(!linea.empty()){		
-			//empaquetador->push_back(procesarLinea(const &linea));
-			empaquetador->agregarPaquete(procesarLinea(const &linea));
-		}
+
+bool getEndiannes(){
+	int16_t i = 1;
+   	int8_t *p = (int8_t *) &i;
+
+   	if (p[0] == 1){
+   		return false;
+   	}
+   	return true;
+}
+
+void ejecutarLogicaBig(){
+	cout <<"logica big\n";
+}
+
+
+void leerNombre(ifstream &arch){
+	char input[2];	
+	char nombre[128];	
+	char remanente = 0x00;
+	int i = 0;
+	do{
+		arch.getline(input,sizeof(input) * 2);		
+		printf("%X\n",input[0]);
+		printf("%X\n",input[1]);
+		if(input[1] == 0x00){
+			nombre[i] = input[1];
+			remanente = input[0];
+			i++;
+		}else{
+			nombre[i] = input[1];
+			i++;
+			nombre[i] = input[0];
+			i++;
+		}		
+	} while((input[0] != 0x00) && (input[1] != 0x00));
+	printf("aca termine");
+	for(int j=0; j < i; j++){
+		printf("%X",nombre[j]);
 	}
+	printf("\n");
+	printf("remanente: %0X\n",remanente);
+}
+
+void parsearArchivo(char *ruta){
+	ifstream arch;
+	arch.open(ruta, std::ifstream::binary);
+//	bool soyBigEndian = getEndiannes();	
+	leerNombre(arch);		
+	cout<< ruta <<":se estable conexion con el dispositivo";
+/*	while(!arch.eof()){			
+		arch.getline(input,sizeof(input) * 4);		
+		memcpy(&magicus,&input,sizeof(magicus));
+		printf("%X\n", magicus);
+		if(magicus == -1){
+			cout <<"ATASCADO MAN\n";
+		}else if(soyBigEndian){
+			ejecutarLogica();			
+		}else{
+			htonl(magicus);
+			ejecutarLogicaBig();
+		}	
+	}*/
+	arch.close();
 }
 
 int main (int argc,char *argv[]){		
 	if (argc < 3){
-		printf("Pelotudo");
+		cout<< "pelotudo\n";
 		return 1;
 	}
 	Empaquetador empaquetador;
 	leerConfiguracion(argv[1],empaquetador);	
 	for(int j = 2; j < argc; j++){
-		parsearArchivo(argv[2],empaquetador);
+		parsearArchivo(argv[j]);
 	}
 	return 0;
 }
